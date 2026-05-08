22564d9fbea1bf27bc7c5e7dc20c4376be472b30
Satish Babariya <satish.babariya@gmail.com>
2026-05-08 14:24:30 +0530

    deps: add swift-bytes 0.1.0 (preparing 0.2 Bytes-output rebase)

--- Package.swift
+++ Package.swift
@@ -11,10 +11,16 @@ let package = Package(
         .library(name: "Prometheus", targets: ["Prometheus"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-docc-plugin.git", from: "1.4.0")
+        .package(url: "https://github.com/swiftlang/swift-docc-plugin.git", from: "1.4.0"),
+        .package(url: "https://github.com/bare-swift/swift-bytes.git", from: "0.1.0")
     ],
     targets: [
-        .target(name: "Prometheus"),
+        .target(
+            name: "Prometheus",
+            dependencies: [
+                .product(name: "Bytes", package: "swift-bytes")
+            ]
+        ),
         .testTarget(
             name: "PrometheusTests",
             dependencies: ["Prometheus"],
